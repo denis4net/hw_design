c89c2c1a265104acecce6ae46cd237e34c9be672
Denis Vashchuk <denis.net@inbox.ru>
2014-10-27 21:45:52 +0300

    Several fixes

--- 3/src/main.v
+++ 3/src/main.v
@@ -1,7 +1,19 @@
+module clkdiv3(input in, output out); // T = CLK_DIVIDER_VALUE of clk, porosity = 1/2
+	wire clk0;
+	reg clk1;
+	
+	BaudRateGenerator sendEveneGenerator(.baudrate(1), .clk(in), .baudclk(clk0));	
+	
+	always @(posedge clk0)
+		clk1 <= ~clk1;
+		
+	assign out = clk1;
+endmodule
+
 module main(
 	input clk,   // connected to pin 152 (50MHz)
 	input rst, 	 // connected to 0 push button
-	input [3:0] baudrate_select, 
+	input [3:0] baudrate_select,
 	input [7:0] data_in, //connected to dip switches
 	output tx,   // connected to led0
 	output rx,
@@ -10,13 +22,14 @@ module main(
 	output [7:0] rx_reg, 
 	output diagnostic
 );
+
+	wire send_event;
+	BaudRateGenerator sendEveneGenerator(.baudrate(1), .clk(clk), .baudclk(send_event));	
 	
-
-  	BaudRateGenerator sendEveneGenerator(.baudrate(1), .clk(clk), .rst(rst), .baudclk(send_event));	
-	assign diagnostic = send_event;
-
-	UART uart0(.tx_busy(tx_busy), .wr_i(send_event), .dat_i(data_in), .rx(rx), .rx_busy(rx_busy), 
-		.rx_reg(rx_reg), .clk(clk), .rst(~rst), .clk_speed_sel(baudrate_select)
+	clkdiv3 c0(.in(clk), .out(diagnostic));
+			
+	UART uart0(.tx_busy(tx_busy), .tx(tx), .wr_i(send_event), .dat_i(data_in), 
+		.rx(rx), .rx_busy(rx_busy), .rx_reg(rx_reg),
+		.clk(clk), .rst(~rst), .clk_speed_sel(~baudrate_select)
 		);
-
 endmodule
--- 3/src/uart_tx_testbench.v
+++ 3/src/uart_tx_testbench.v
@@ -11,16 +11,19 @@ module testbench;
   wire rx_busy;
   wire [7:0] rx_reg;
 
+  wire bg_clk;
+
   initial begin
   	$dumpfile("uart_testbench.vcd");
     $dumpvars(0, uart0);
 
     rx <= 1;
     sys_clk <= 0;
-    sys_rst <= 1;
+    sys_rst <= 0;
     uart_dat_i <= 8'hFF;
     uart_wr_i <= 0;
-    
+
+    #1 sys_rst <= 1;
     #4 sys_rst <= 0;
     #2 uart_wr_i <= 1;
     #4 uart_wr_i <= 0;
@@ -41,28 +44,11 @@ module testbench;
         #1 sys_clk <= ~sys_clk;
   end
   
-/*
-module uart(
-   // Transmiter part
-   output tx_busy,   // High means UART is transmitting
-   output reg tx,     // UART transmit wire
-   input wr_i,   // Raise to transmit byte
-   input [7:0] dat_i,  // 8-bit data
-
-   //Receiver part
-   input rx,
-   output rx_busy,
-   output reg [7:0] rx_reg,
-
-   input clk,   // System clock, 50 MHz
-   input rst,    // System reset
-
-   input [3:0] clk_speed_sel
-);*/
-
   UART#(.BAUD_RATE(1), .SYS_CLK_RATE(2)) uart0(
       .tx_busy(uart_busy), .tx(uart_tx), .wr_i(uart_wr_i), .dat_i(uart_dat_i), 
       .clk(sys_clk), .rst(sys_rst), 
       .rx(rx), .rx_busy(rx_busy), .rx_reg(rx_reg)
     ); 
+
+  BaudRateGenerator#(.SYS_CLK_RATE(5)) generator(.baudrate(1), .clk(sys_clk), .rst(sys_rst), .baudclk(bg_clk)); 
 endmodule
